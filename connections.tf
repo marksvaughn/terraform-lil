5f03d9508efa14803ac26d413485ff744c460277
mark.vaughn <mark.vaughn@infusionsoft.com>
2018-06-15 08:02:15 -0700

    Terraform init

---
+++ connections.tf
@@ -0,0 +1,18 @@
+
+
+provider "google" {
+    credentials = "${file("../account.json")}"
+    project = "My First Project"
+    region = "us-west1"
+}
+
+provider "aws"
+    region = "us-west-2"
+}
+
+provider "azurerm" {
+    subscription_id = "0"
+    client_id = "1"
+    client_secret = "2"
+    tenant_id = "3"
+}
